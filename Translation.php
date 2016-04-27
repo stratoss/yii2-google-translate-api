25b01ccafb6d1763ec730b61cd8d73359fb96304
Stanimir Stoyanov <stanimir@datacentrix.org>
2016-04-27 18:03:18 +0300

    Update Translation.php

--- Translation.php
+++ Translation.php
@@ -1,8 +1,9 @@
 <?php
 /**
  * @copyright Copyright &copy; Roman Bahatyi, richweber.net, 2015
+ * @copyright Copyright &copy; Stanimir Stoyanov, 2016
  * @package yii2-google-translate-api
- * @version 1.0.0
+ * @version 1.1.2
  */
 
 namespace stratoss\google\translate;
@@ -16,6 +17,8 @@ use yii\helpers\Html;
  * @link https://cloud.google.com/translate/v2/using_rest
  * @author Roman Bahatyi <rbagatyi@gmail.com>
  * @since 1.0
+ * @author Stanimir Stoyanov <stanimir@datacentrix.org>
+ * @since 1.1
  */
 class Translation
 {
@@ -35,11 +38,15 @@ class Translation
      * to another language
      * @param string $source Source language
      * @param string $target Target language
-     * @param string $text   Source text string
+     * @param string|array $text   Source text string/array of strings
+     * @throws \Exception if the translated text is neither string or array
      * @return array
      */
     public function translate($source, $target, $text)
     {
+        if (gettype($text) !== 'string' && gettype($text) !== 'array') {
+            throw new \Exception("The translated text must be either string or array");
+        }
         return $this->getResponse($this->getRequest('', $text, $source, $target));
     }
 
@@ -65,21 +72,33 @@ class Translation
     /**
      * Forming query parameters
      * @param  string $method API method
-     * @param  string $text   Source text string
+     * @param  string|array $text   Source text string
      * @param  string $source Source language
      * @param  string $target Target language
      * @return array          Data properties
+     * @see https://cloud.google.com/translate/v2/using_rest#Translate for multiple translations
      */
     protected function getRequest($method, $text = '', $source = '', $target = '')
     {
+        if (gettype($text) === 'string') {
+            $text = [$text];
+        }
+
+        foreach ($text as $textKey => $textValue) {
+            $text[$textKey] = rawurlencode($textValue);
+        }
+
         $request = self::API_URL . '/' . $method . '?' . http_build_query(
             [
                 'key' => $this->key,
                 'source' => $source,
                 'target' => $target,
-                'q' => Html::encode($text),
             ]
         );
+
+        foreach ($text as $string) {
+            $request .= '&q=' . $string;
+        }
 
         return $request;
     }
